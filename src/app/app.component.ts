5594e838569a321f1e9d2de5a608996b8aa4a47f
Denis Villagrán <denisvillagran99@gmail.com>
2021-04-07 14:34:16 -0600

    feat: form to add persons with validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Persona } from './models/persona.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Listado de Personas';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('frmAddPerson') frmAddPerson: NgForm | undefined;
+
+  titulo = 'Listado de Personas';
+  personas: Persona[] = [
+    new Persona('Juan', 'Pérez'),
+    new Persona('Laura', 'Juárez'),
+    new Persona('Karla', 'Lara'),
+  ];
+  persona: Persona;
+
+  constructor() {
+    this.persona = this.getNewPersona();
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.frmAddPerson);
+  }
+
+  getNewPersona(nombre?: string, apellido?: string): Persona {
+    return new Persona(nombre || '', apellido || '');
+  }
+
+  agregarPersona(): void {
+    if (this.frmAddPerson?.invalid) return;
+    var persona = this.getNewPersona(this.persona.nombre, this.persona.apellido);
+    this.personas.push(persona);
+    this.persona = this.getNewPersona();
+    this.frmAddPerson?.form.reset();
+  }
 }
